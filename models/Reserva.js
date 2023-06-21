2cee52ca6c49780eb4bbd68368b448a4e356bd28
Fabian <fabianvillagra28@gmail.com>
2023-06-21 15:05:24 -0300

    Se agrago la tabla de la base de datos con la abstraccion de lo que se solicitara y se agrego el archivo README

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,52 @@
 // TODO: Crear modelo de datos de Reserva
+
+const { sequelize, DataTypes } = require('../db');
+
+const reserva = sequelize.define('reserva', {
+
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    telefono: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    fecha: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reservas'
+});
+
+
+reserva.sync();
+
+module.exports = reserva;   
